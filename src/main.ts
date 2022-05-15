d6c6958b25539868626854ef4f87f3fe12448096
BiFoS <jon89071@gmail.com>
2022-05-16 01:26:42 +0900

    1.2 샘플 컨트롤러 추가 및 Fastify Type 적용

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter({
+      // console에 찍혀서 별로 보기 안좋음
+      // NestLogger를 사용하자
+      //logger: true
+    }),
   );
   await app.listen(3000, '0.0.0.0');
 }
--- src/module/sample/sample.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Res } from '@nestjs/common';
-
-@Controller()
-export class SampleController {
-  @Get('/sample')
-  index(@Res() res) {
-    res.status(302).redirect('/login');
-  }
-}
