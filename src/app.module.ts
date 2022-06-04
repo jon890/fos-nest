6ba262903cf22ff819c14325712b511ff71070e7
BiFoS <jon89071@gmail.com>
2022-06-04 17:53:10 +0900

    프로바이더 공부 및  fastify  패키지 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleController } from './sample/sample.controller';
+import SampleService from './sample/sample.service';
 
 @Module({
   imports: [],
   controllers: [AppController, SampleController],
-  providers: [AppService],
+  providers: [AppService, SampleService],
 })
 export class AppModule {}
--- src/sample/sample.controller.ts
+++ src/sample/sample.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import SampleService from './sample.service';
 
 @Controller('/sample')
 export class SampleController {
+  constructor(private readonly sampleService: SampleService) {}
+
   @Get()
   async findAll(
     @Req() request: FastifyRequest,
