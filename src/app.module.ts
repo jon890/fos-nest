96f2199a6006b66593a3c424ba6364936f1fb060
BiFoS <jon89071@gmail.com>
2022-09-04 23:41:12 +0900

    - :construction_worker: Setup Docker Compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleController } from './sample/sample.controller';
 import SampleService from './sample/sample.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'bifos',
+      password: 'bifos',
+      database: 'bifos_db',
+      models: [],
+    }),
+  ],
   controllers: [AppController, SampleController],
   providers: [AppService, SampleService],
 })
